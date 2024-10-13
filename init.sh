5cfc136ae76e8da4a196c1f683a7b2f4d68b2b21
chn0318 <1623499602@qq.com>
2024-10-13 03:20:20 -0700

    upgrade init.sh

--- init.sh
+++ init.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
-set -e 
+set -e
+echo "---------- Updating package list ----------"
 sudo apt update
+echo "---------- Installing Go ----------"
 cd /usr/local 
 sudo wget https://go.dev/dl/go1.23.1.linux-arm64.tar.gz
 sudo tar -C /usr/local -xzf go1.23.1.linux-arm64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 export GOPATH=~/Project-Go
 export PATH=$PATH:$GOPATH/bin
+echo "---------- Installing goreman ----------"
 go install github.com/mattn/goreman@latest
 cd ~
+echo "---------- Downloading and Complie scalog ----------"
 mkdir -p ./Project-Go/src/github.com/scalog
 cd ./Project-Go/src/github.com/scalog
 git clone https://github.com/chn0318/scalog.git
 cd ./scalog
 export GO111MODULE=off
 go build
+echo "---------- Adding Env var ----------"
 echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
 echo 'export GOPATH=~/Project-Go' >> ~/.bashrc
 echo 'export PATH=$PATH:$GOPATH/bin' >> ~/.bashrc
-source ~/.bashrc
