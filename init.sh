8b6ad2eab7b985910b3be2f94b2833ce92b7819e
chn0318 <1623499602@qq.com>
2024-12-10 02:12:48 -0800

    change ARCH version for go

--- init.sh
+++ init.sh
@@ -4,8 +4,8 @@ echo "---------- Updating package list ----------"
 sudo apt update
 echo "---------- Installing Go ----------"
 cd /usr/local 
-sudo wget https://go.dev/dl/go1.23.1.linux-arm64.tar.gz
-sudo tar -C /usr/local -xzf go1.23.1.linux-arm64.tar.gz
+sudo wget https://go.dev/dl/go1.23.1.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.23.1.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 export GOPATH=~/Project-Go
 export PATH=$PATH:$GOPATH/bin
