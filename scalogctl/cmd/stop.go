81c81c4aa4ac09108c2084ba4b3638d419a1a570
chn0318 <1623499602@qq.com>
2025-10-21 06:59:00 -0700

    change scalogctl name

--- scalogctl/cmd/stop.go
+++ scalogctl/cmd/stop.go
@@ -5,8 +5,8 @@ import (
 	"strings"
 	"sync"
 
-	"github.com/chn0318/scalogctl/pkg/config"
-	"github.com/chn0318/scalogctl/pkg/plan"
+	"github.com/scalog/scalog/scalogctl/pkg/config"
+	"github.com/scalog/scalog/scalogctl/pkg/plan"
 	"github.com/spf13/cobra"
 )
 
